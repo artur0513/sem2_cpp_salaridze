45214a9def0c9def4852c847bd0b20ced1aa853c
artur0513 <artur.salaridze@yandex.ru>
2022-03-01 22:46:20 +0300

    upd crystal

--- 15.02.2022_crystal/main.cpp
+++ 15.02.2022_crystal/main.cpp
@@ -14,13 +14,17 @@ public:
         y = _y;
     }
 };
-
+/*
+ЗАДАНИЕ: 1)добавить возможность дислокации пролетать через стенки и вылетать с другой стороны по одной из осей
+2) снять больше тестов для второго задания с разными размерами и лианеризовать зависимость как нибудь
+*/
 class Crystal{ // сам класс для кристаллов
 public:
     int crystal_size_x, crystal_size_y, number_of_disloc;
-    vector<vector<int>> crystal; // двумерный массив кристалла
+    vector<vector<short>> crystal; // двумерный массив кристалла
 
-    vector<Vec2i> moving_disloc; // координаты дислокаций, которые можно
+    vector<Vec2i*> moving_disloc; // ссылки координаты дислокаций, которые можно двигать
+    vector<Vec2i> disloc; // координаты дислокаций
 
     int turn_num = 0; // счетчик ходов
     bool process_ended = false; // закончился ли процесс
@@ -30,7 +34,7 @@ public:
         crystal_size_y = _crystal_size_y;
         number_of_disloc = _number_of_disloc;
         for (int i = 0; i < _crystal_size_x; i++){
-            crystal.push_back(vector<int>(_crystal_size_y, 0));
+            crystal.push_back(vector<short>(_crystal_size_y, 0));
         }
         generate_disloc();
     }
@@ -42,6 +46,7 @@ public:
                 x = rand() % crystal_size_x;
                 y = rand() % crystal_size_y;
             } while (crystal[x][y] != 0);
+            disloc.push_back(Vec2i(x, y));
             crystal[x][y] = 1;
         }
     }
@@ -77,12 +82,13 @@ public:
     }
 
     void get_moving_dislocs(){ // ищем и составляем массив с координатами дислокаций, которые будут дивгаться
-        for (int y = 0; y < crystal_size_y; y++){ // Здесь ГОРАЗДО логичнее и быстрее было бы хранить отдельный массив со всеми дислокациями
-            for (int x = 0; x < crystal_size_x; x++){ // а не перебирать весь двумерный массив, но мне лень переделывать
+        for (int i = 0; i < disloc.size(); i++){ // Здесь ГОРАЗДО логичнее и быстрее было бы хранить отдельный массив со всеми дислокациями
+                int x = disloc[i].x;
+                int y = disloc[i].y;
                 if (crystal[x][y] == 1 && check_move(x, y)){
-                    moving_disloc.push_back(Vec2i(x, y));
+                    moving_disloc.push_back(&disloc[i]);
                 }
-            }
+
         }
     }
 
@@ -95,8 +101,8 @@ public:
             else
                 dir = rand()%2; // если одномерный кристалл, то движемся только в двух направлениях
 
-            x = moving_disloc[i].x;
-            y = moving_disloc[i].y;
+            x = moving_disloc[i]->x;
+            y = moving_disloc[i]->y;
             crystal[x][y] = 0;
 
             if (dir == 0){
@@ -105,6 +111,7 @@ public:
                 }
                 else{
                     crystal[x+1][y] = 1;
+                    moving_disloc[i]->x += 1;
                 }
             }
 
@@ -114,6 +121,7 @@ public:
                 }
                 else{
                     crystal[x-1][y] = 1;
+                    moving_disloc[i]->x -= 1;
                 }
             }
 
@@ -123,6 +131,7 @@ public:
                 }
                 else{
                     crystal[x][y+1] = 1;
+                    moving_disloc[i]->y += 1;
                 }
             }
 
@@ -132,6 +141,7 @@ public:
                 }
                 else{
                     crystal[x][y-1] = 1;
+                    moving_disloc[i]->y -= 1;
                 }
             }
 
@@ -146,6 +156,9 @@ public:
                 process_ended = true;
             move_all_disloc();
             turn_num++;
+            if (turn_num > 200000){ // Тут можно поставить ограничение количество ходов, если тест идет слишком долго
+                process_ended = true;
+            }
         }
         return process_ended;
     }
@@ -196,7 +209,7 @@ float second_task_test(int number_of_tests, int crystal_size, float koeff, bool 
 }
 
 void second_task_full_test(bool third_test = false, float min_percent = 0.02, float max_percent = 0.98, float step = 0.02, int num_of_simulations = 500){
-    int crystal_size = 50;
+    int crystal_size = 100;
     if (third_test)
         crystal_size = 200;
     for (float i = min_percent; i < max_percent; i+=step){
@@ -208,5 +221,5 @@ int main()
 {
 srand(time(0));
 
-second_task_full_test(true);
+second_task_full_test();
 }
